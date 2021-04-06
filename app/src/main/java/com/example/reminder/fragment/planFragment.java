2b1be2ddbd52a5ec2c6160888e88b5f5cf65f893
只猫 <1796861552@qq.com>
2021-04-06 16:22:25 +0800

    update

--- app/src/main/java/com/example/reminder/fragment/planFragment.java
+++ app/src/main/java/com/example/reminder/fragment/planFragment.java
@@ -1,5 +1,6 @@
 package com.example.reminder.fragment;
 
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.example.reminder.R;
@@ -24,6 +26,17 @@ public class planFragment extends Fragment {
         planItemAdapter adapter=new planItemAdapter(getActivity(), R.layout.plan_item_layout,planList);
         ListView listView=(ListView) view.findViewById(R.id.plan_list_view);
         listView.setAdapter(adapter);
+
+        Dialog dialog=new Dialog(getActivity());
+        dialog.setContentView(R.layout.information_input_layout);
+
+        Button inputButton=view.findViewById(R.id.inputButton);
+        inputButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
         return view;
     }
 
